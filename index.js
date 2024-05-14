9960b38626ec839f8a3c8d1ba44fcab1bb45a6c8
trieu1212 <lequoctrieu618@gmail.com>
2024-05-14 08:45:43 +0700

    rest api products

--- index.js
+++ index.js
@@ -1,56 +1,85 @@
 const express = require('express');
-const cors = require('cors');
-const WooCommerceAPI = require('woocommerce-api');
+// const WooCommerceAPI = require('woocommerce-api');
 const app = express();
-app.use(cors());
-
-const WooCommerce = new WooCommerceAPI({
+const WooCommerceRestApi = require("@woocommerce/woocommerce-rest-api").default;
+const WooCommerce = new WooCommerceRestApi({
     url: 'https://bookworm.io.vn',
     consumerKey: 'ck_2449dd439b7fcfbb69475a45988f5a77d809a032',
     consumerSecret: 'cs_bcb785842b064585c5852853ec02917bf3c91b7f',
     wpAPI: true,
-    version: 'wc/v3' // Updated version to 'wc/v3' as 'wc/v1' is deprecated
+    version: 'wc/v3',
+    queryStringAuth: true
 });
 
-const product = {
-    name: "New Product",
+const data = {
+    name: "Premium Quality",
     type: "simple",
-    regular_price: "19.99",
-    description: "This is a new product",
+    regular_price: "21.99",
+    description: "Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.",
+    short_description: "Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.",
     categories: [
-        {
-            id: 9
-        }
+      {
+        id: 9
+      },
+      {
+        id: 14
+      }
+    ],
+    images: [
+      {
+        src: "http://demo.woothemes.com/woocommerce/wp-content/uploads/sites/56/2013/06/T_2_front.jpg"
+      },
+      {
+        src: "http://demo.woothemes.com/woocommerce/wp-content/uploads/sites/56/2013/06/T_2_back.jpg"
+      }
     ]
-};
+  };
 const getProduct = async () => {
-    try {
-        const response = await WooCommerce.get('products',(err,res)=>{
-            if(err){
-                console.log(err)
-            }
-            console.log(res)
-        })
-        console.log(JSON.stringify(response.body))
-    } catch (error) {
-        console.error(error.response.data);
-    }
+    WooCommerce.get("products")
+  .then((response) => {
+    console.log(response.data);
+  })
+  .catch((error) => {
+    console.log(error.response.data);
+  });
 };
 const postProduct = async () => {
-    try {
-        const response = await WooCommerce.post('/products',product,(err,res)=>{
-            if(err){
-                console.log(err)
-            }
-            console.log('thanh cong')
-        })
-    } catch (error) {
-        console.error(error.response.data);
-    }
+    WooCommerce.post("products", data)
+  .then((response) => {
+    console.log(response.data);
+  })
+  .catch((error) => {
+    console.log(error.response.data);
+  });
 };
-
+const updateProduct = async () => {
+    const data = {
+        regular_price: "24.54"
+      };
+      
+      WooCommerce.put("products/596", data)
+        .then((response) => {
+          console.log(response.data);
+        })
+        .catch((error) => {
+          console.log(error.response.data);
+        });
+}
+const deleteProduct = async () =>{
+    WooCommerce.delete("products/789", {
+        force: true
+      })
+        .then((response) => {
+          console.log(response.data);
+        })
+        .catch((error) => {
+          console.log(error.response.data);
+        });
+}
 app.listen(9000, () => {
     console.log('Server is running on port 9000');
     // postProduct();
-    getProduct()
+    // getProduct()
+    // updateProduct()
+    deleteProduct()
 });
